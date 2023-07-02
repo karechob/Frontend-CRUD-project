7e0c2d4f9f247708bdbea11f150610291d308635
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-02 19:47:20 -0400

    adding routing to app.js

--- crud-frontend/src/App/App.js
+++ crud-frontend/src/App/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from '../pages/Home';
+import Campuses from '../pages/Campuses';
+import Students from '../pages/Students';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className='App'>
+      <Home/>
     </div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/allstudents' element={<Students/>} />
+      <Route path='/allcampuses' element={<Campuses/>} />
+      {/* <Route path='/student' element={<SingleStudent/>} />
+      <Route path='/campus' element={<SingleCampus/>} /> */}
+    </Routes>
+    </Router>
   );
 }
 
