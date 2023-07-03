c78c7bda66e3c449ea093f13f08d96e945dca889
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-03 13:14:42 -0400

    fixed imports for campus and students.jsx

--- crud-frontend/src/pages/Students.jsx
+++ crud-frontend/src/pages/Students.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navigation from '../components/Navigation';
 import { useDispatch, useSelector } from "react-redux";
 import ListingStudents from "../components/ListingStudents";
