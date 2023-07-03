6cc7d8207771b58bdaf195d15123eedb1cbe4785
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-03 10:55:06 -0400

    reworked files

--- crud-frontend/src/pages/Campuses.jsx
+++ crud-frontend/src/pages/Campuses.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 import Navigation from '../components/Navigation'
 
 function Campuses() {
-  const allShoes = useSelector((state) => state.shoes.allShoes);
+  const allCampuses = useSelector((state) => state.shoes.allShoes);
   const dispatch = useDispatch();
 
-  function fetchAllShoes() {
-    console.log('RUNNING DISPATCH FROM FETCHALLSHOES');
+  function fetchAllCampuses() {
+    console.log('RUNNING DISPATCH FROM FETCHALLCAMPUSES');
     return dispatch(fetchAllShoesThunk());
   }
 
   useEffect(() => {
     console.log('FETCH ALL SHOES FIRING IN USEEFFECT');
-    fetchAllShoes();
+    fetchAllCampuses();
   }, []);
 
   return (
     <div>
         <Navigation/>
          Campuses
+         <h1>Shoes Page</h1>
+        <ListItems list={allCampuses} />
     </div>
   )
 }
--- crud-frontend/src/pages/Students.jsx
+++ crud-frontend/src/pages/Students.jsx
@@ -2,25 +2,27 @@ import React from 'react'
 import Navigation from '../components/Navigation'
 
 function Students() {
-  const allShoes = useSelector((state) => state.shoes.allShoes);
+  const allStudents = useSelector((state) => state.students.allStudents);
   const dispatch = useDispatch();
 
-  function fetchAllShoes() {
-    console.log('RUNNING DISPATCH FROM FETCHALLSHOES');
+  function fetchAllStudents() {
+    console.log('RUNNING DISPATCH FROM FETCHALLSTUDENTS');
     return dispatch(fetchAllShoesThunk());
   }
 
   useEffect(() => {
     console.log('FETCH ALL SHOES FIRING IN USEEFFECT');
-    fetchAllShoes();
+    fetchAllStudents();
   }, []);
 
   return (
     <div>
       <Navigation/>
       Students
+      <h1>Students Page</h1>
+      <ListItems list={allStudents} />
     </div>
   )
 }
 
-export default Students
+export default Students;
