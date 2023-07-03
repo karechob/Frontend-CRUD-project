291f09990f67dec6ceed2d3973baf692e13bcdc8
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-03 13:55:53 -0400

    trying to fix issues with store.js imports

--- crud-frontend/src/redux/store.js
+++ crud-frontend/src/redux/store.js
@@ -1,18 +1,20 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import axios from 'axios'
 import { createLogger } from 'redux-logger'
-import { thunkMiddleware } from 'redux-thunk'
+import thunk from 'redux-thunk' // Import thunk from 'redux-thunk'
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from './root-reducer'
+import { applyMiddleware } from "redux";
 
 const logger = createLogger({ collapsed: true })
 
-const middleware = [...getDefaultMiddleware(), thunkMiddleware.withExtraArgument({ axios }), logger];
+const middleware = [...getDefaultMiddleware(), thunk.withExtraArgument({ axios }), logger];
+// Use thunk.withExtraArgument
 
 const store = configureStore({
-    reducer: rootReducer,
-    middleware,
-    devTools: composeWithDevTools()
-  });
+  reducer: rootReducer,
+  middleware,
+  devTools: composeWithDevTools()
+});
 
 export default store;
