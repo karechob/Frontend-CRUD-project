56c289bc36d2a3baeced3c684348456f15083ef9
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-03 11:56:56 -0400

    added newer version of createStore

--- crud-frontend/src/redux/store.js
+++ crud-frontend/src/redux/store.js
@@ -1,7 +1,18 @@
-import {applyMiddleware, createStore} from 'redux'
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import axios from 'axios'
-import {createLogger} from 'redux-logger'
-import {ThunkMiddleware} from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { createLogger } from 'redux-logger'
+import { thunkMiddleware } from 'redux-thunk'
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from './root-reducer'
 
+const logger = createLogger({ collapsed: true })
+
+const middleware = [...getDefaultMiddleware(), thunkMiddleware.withExtraArgument({ axios }), logger];
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware,
+    devTools: composeWithDevTools()
+  });
+
+export default store;
