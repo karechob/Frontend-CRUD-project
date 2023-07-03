4d1eaccab4f568a2f1d7533fd1c73bcba8152c61
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-03 12:23:48 -0400

    fixed typo in reducer and exported campus.type

--- crud-frontend/src/redux/campuses/campuses.reducer.js
+++ crud-frontend/src/redux/campuses/campuses.reducer.js
@@ -7,7 +7,7 @@ export const INITIAL_CAMPUS_STATE = {
 const campusReducer = (state = INITIAL_CAMPUS_STATE, {type, payload} ) => {
     console.log('CAMPUSREDUCER IS HANDLING FETCH ALL CAMPUSES ACTION')
     switch(type) {
-        case CampusesActionType.FETCH_ALL_CAMPUSES;
+        case CampusesActionType.FETCH_ALL_CAMPUSES:
         return {...state, allCampuses: payload};
         default:
             return state;
---
+++ crud-frontend/src/redux/campuses/campuses.types.js
@@ -0,0 +1,4 @@
+const CampusesActionType = {
+    FETCH_ALL_CAMPUSES: "FETCH_ALL_CAMPUSES",
+}
+export default CampusesActionType;
