c9d2f9c715022a4199723e4c1ff400a5b7b17e3b
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-05 19:58:54 -0400

    removed unnecessary link

--- crud-frontend/src/pages/Students.jsx
+++ crud-frontend/src/pages/Students.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import Navigation from '../components/Navigation';
 import { useDispatch, useSelector } from "react-redux";
 import ListingStudents from "../components/ListingStudents";
-import { Link } from "react-router-dom";
 import { fetchAllStudentsThunk } from '../redux/students/students.actions';
 
 function Students() {
