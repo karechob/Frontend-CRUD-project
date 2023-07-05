9bf880ef9f1629c085f69dee0a85978e041c9b47
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-05 19:58:43 -0400

    forcefully rerender

--- crud-frontend/src/pages/Students.jsx
+++ crud-frontend/src/pages/Students.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navigation from '../components/Navigation';
-import { useDispatch, useSelector } from "react-redux";
-// import ListingStudents from "../components/ListingStudents";
-import { Link } from "react-router-dom";
-import { fetchAllStudentsThunk } from '../redux/students/students.actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchAllStudentsThunk, deleteStudentThunk } from '../redux/students/students.actions';
 
 function Students() {
   const allStudents = useSelector((state) => state.students.allStudents);
-  console.log(" THIS IS ALL STUDENTS ->  ",allStudents);
   const dispatch = useDispatch();
+  const [rerender, setRerender] = useState(false);
 
   function fetchAllStudents() {
     console.log('RUNNING DISPATCH FROM FETCHALLSTUDENTS');
@@ -18,24 +17,27 @@ function Students() {
   useEffect(() => {
     console.log('FETCH ALL STUDENTS FIRING IN USEEFFECT');
     fetchAllStudents();
-  }, []);
+  }, [dispatch, rerender]);
+
+  const handleDeleteStudent = (studentId) => {
+    dispatch(deleteStudentThunk(studentId));
+    setRerender(!rerender);
+  };
 
   return (
     <div>
-      <Navigation/>
-      Students
+      <Navigation />
       <h1>Students Page</h1>
       {allStudents.map((student) => (
         <div key={student.id}>
           <h2>{student.firstName} {student.lastName}</h2>
-          <img src={student.imageUrl} alt={`${student.firstName} ${student.lastName}`}/>
-          <p>Address: {student.address}</p>
-          <p>Description: {student.description}</p>
+          <img src={student.imageUrl} alt={`${student.firstName} ${student.lastName}`} />
           <Link to={`/student/${student.id}`}>View Student</Link>
+          <button onClick={() => handleDeleteStudent(student.id)}>X</button>
         </div>
       ))}
     </div>
-  )
+  );
 }
 
 export default Students;
