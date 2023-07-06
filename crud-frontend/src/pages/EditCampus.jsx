7a728337b0fe1e8a24e9f4f3749dae4525ae3c8c
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-06 17:00:46 -0400

    campus edit

--- crud-frontend/src/pages/EditCampus.jsx
+++ crud-frontend/src/pages/EditCampus.jsx
@@ -1,16 +1,111 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
+import { updateCampusThunk } from '../redux/campuses/campuses.actions';
 
 
 function EditCampus() {
 
+  const { campusId } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const singleCampus = useSelector((state) => state.campuses.singleCampus);
 
+  const [formData, setFormData] = useState({
+    name: '',
+    address: '',
+    description: '',
+  });
+  const [errors, setErrors] = useState({});
 
+  useEffect(() => {
+        if (singleCampus) {
+          setFormData({
+            name: singleCampus.name,
+            address: singleCampus.address,
+            description: singleCampus.description,
+          });
+        }
+  }, [singleCampus, dispatch]);
 
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
 
+  const validateForm = () => {
+    let formErrors = {};
+    if (formData.name.trim() === '') {
+      formErrors.firstName = 'Name is required.';
+    }
+    if (formData.address.trim() === '') {
+      formErrors.address = 'Address is required.';
+    }
+    if (formData.description.trim() === '') {
+      formErrors.description = 'Description is required.';
+    }
+    setErrors(formErrors);
+    return Object.keys(formErrors).length === 0;
+  };
 
-   
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      const updatedCampus = {
+        id: campusId,
+        address: formData.address,
+        campusId: formData.campusId,
+      };
+      dispatch(updateCampusThunk(updatedCampus))
+        .then(() => {
+          navigate(`/campuses/${campusId}`);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
   return (
-    <div>AddStudent</div>
+    <div>
+      <h2>Edit Student</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          {errors.name && <p>{errors.name}</p>}
+        </div>
+        <div>
+          <label>Address:</label>
+          <input
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleInputChange}
+          />
+          {errors.address && <p>{errors.address}</p>}
+        </div>
+        <div>
+          <label>Description:</label>
+          <input
+            type="text"
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+          />
+          {errors.description && <p>{errors.description}</p>}
+        </div>
+        <button type="submit" >Save</button>
+      </form>
+    </div>
   )
 }
 
