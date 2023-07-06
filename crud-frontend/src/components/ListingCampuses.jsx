93ed97abdd2de3fae07f64bc6ed05afc1a7941bb
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-06 18:13:17 -0400

    fixed delete campus on all campuses view

--- crud-frontend/src/components/ListingCampuses.jsx
+++ crud-frontend/src/components/ListingCampuses.jsx
@@ -3,21 +3,29 @@ import { Link } from "react-router-dom";
 
 export default function ListingCampuses(props) {
   console.log("LIST CAMPUSES COMPONENT");
+  
+  const handleRemoveCampus = (campusId) => {
+    props.handleRemoveCampus(campusId);
+    window.location.reload();
+  };
+
   return props.list ? (
     props.list.map((item) => {
       return (
         <div className="campus-grid">
-          <Link className="card-link" to={`/campuses/${item.id}`}>
           <div className="container-campus" key={item.id}>
             <div className="campus-pic">
               <img src={item.imageUrl} alt={item.name} />
             </div>
-            <h1>{item.name}</h1>
-              <button onClick={() => props.handleRemoveCampus(item.id)}>
+            {/* <h1>{item.name}</h1> */}
+            <Link className="card-link" to={`/campuses/${item.id}`}>
+              {" "}
+              <h1>{item.name}</h1>{" "}
+            </Link>
+            <button onClick={() => handleRemoveCampus(item.id)}>
               Remove Campus
             </button>
-            </div>
-          </Link>
+          </div>
         </div>
       );
     })
