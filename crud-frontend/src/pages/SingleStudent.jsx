33a08d7fcca899c7e2bd11eb9e1c0c877c9e9057
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-06 21:49:12 -0400

    styled single student view

--- crud-frontend/src/pages/SingleStudent.jsx
+++ crud-frontend/src/pages/SingleStudent.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import Navigation from '../components/Navigation';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchSingleStudentThunk, deleteStudentThunk, updateStudentThunk } from '../redux/students/students.actions';
-import { useParams, useNavigate, Link } from 'react-router-dom';
-import EditStudent from './EditStudent';
+import React, { useEffect, useState } from "react";
+import Navigation from "../components/Navigation";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchSingleStudentThunk,
+  deleteStudentThunk,
+  updateStudentThunk,
+} from "../redux/students/students.actions";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import EditStudent from "./EditStudent";
 
 function SingleStudent() {
   const [forceRerenderKey, setForceRerenderKey] = useState(0);
@@ -29,42 +33,50 @@ function SingleStudent() {
   return (
     <div key={forceRerenderKey}>
       <Navigation />
-      <h1>Student</h1>
+      <h1 className="campus-title">Student</h1>
 
       {singleStudent ? (
         <div>
-          <h2>
+          <h2 className="campus-title">
             {singleStudent.firstName} {singleStudent.lastName}
           </h2>
-          <img src={singleStudent.imageUrl} alt={singleStudent.firstName} />
+          <div className="image-container-single-view">
+            <img
+              className="campus-img-single-view"
+              src={singleStudent.imageUrl}
+              alt={singleStudent.firstName}
+            />
+          </div>
           <div>
-            <p>Gpa: {singleStudent.gpa}</p>
-            <p>Email: {singleStudent.email}</p>
-            {singleStudent.campus ? (
-              <div>
-                <h2>Currently Attending</h2>
-                <p>{singleStudent.campus.name}</p>
-                <Link to={`/campuses/${singleStudent.campus.id}`}>
-                  View Campus
-                </Link>
-              </div>
-            ) : (
-              <p>Not enrolled at any campus.</p>
-            )}
+            <p className="paragraph-img">Gpa: {singleStudent.gpa}</p>
+            <p className="paragraph-img">Email: {singleStudent.email}</p>
             <div>
               {isEditing ? (
                 <EditStudent />
               ) : (
-                <div>
-                  <button onClick={handleToggleEdit}>Edit</button>
+                <div className="btn-container">
+                  <button onClick={handleToggleEdit}>Edit Student</button>
+                  <button className="btn-campus-delete" nClick={handleDeleteStudent}>X</button>
                 </div>
               )}
             </div>
+            {singleStudent.campus ? (
+              <div>
+                <h1 className="campus-title">Currently Attending</h1>
+                <h2 className="campus-title">
+                  <Link className="link-color" to={`/campuses/${singleStudent.campus.id}`}>
+                    {singleStudent.campus.name}
+                  </Link>
+                  <button className="btn-campus-delete" onClick={handleDeleteStudent}>X</button>
+                </h2>
+              </div>
+            ) : (
+              <p className="campus-title">Not enrolled at any campus.</p>
+            )}
           </div>
-          <button onClick={handleDeleteStudent}>X</button>
         </div>
       ) : (
-        <p>There is not information for this student currently</p>
+        <p className="campus-title">There is not information for this student currently</p>
       )}
     </div>
   );
