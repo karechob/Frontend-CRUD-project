ae82fd9608fae94f0206bf3bf39538f8fcd304fc
admin <admin@users-MacBook-Pro.local>
2023-07-07 11:15:57 -0400

    all alerts up and running

--- crud-frontend/src/App/App.js
+++ crud-frontend/src/App/App.js
@@ -9,6 +9,44 @@ import AddCampus from '../pages/AddCampus';
 import AddStudent from '../pages/AddStudent';
 import EditStudent from '../pages/EditStudent';
 import EditCampus from '../pages/EditCampus';
+
+function App() {
+  return (
+    <Router>
+      {/* <div className='App'>
+        <Home />
+      </div> */}
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route exact path='/students' element={<Students />} />
+        <Route exact path='/campuses' element={<Campuses />} />
+        <Route path="/campuses/:campusId" Component={SingleCampus} />
+        <Route path="/student/:studentId" Component={SingleStudent} />
+        <Route path='/student' element={<AddStudent />} />
+        <Route path='/campus' element={<AddCampus />} />
+        <Route path="/student/:studentId" element={<EditStudent />} />
+        <Route path="/campus/:campusId" element={<EditCampus />} />
+      </Routes>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+      </style>
+    </Router>
+  );
+}
+
+export default App;
+/*
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import Home from '../pages/Home';
+import Campuses from '../pages/Campuses';
+import Students from '../pages/Students';
+import SingleCampus from '../pages/SingleCampus';
+import SingleStudent from '../pages/SingleStudent';
+import AddCampus from '../pages/AddCampus';
+import AddStudent from '../pages/AddStudent';
+import EditStudent from '../pages/EditStudent';
+import EditCampus from '../pages/EditCampus';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -18,7 +56,7 @@ function App() {
     <Router>
       {/* <div className='App'>
         <Home />
-      </div> */}
+      </div> }
       <Routes>
         <Route path='/home' element={<Home />} />
         <Route exact path='/students' element={<Students />} />
@@ -38,3 +76,4 @@ function App() {
 }
 
 export default App;
+*/
